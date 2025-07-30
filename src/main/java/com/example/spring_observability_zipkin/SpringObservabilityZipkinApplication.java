4f7543eca16f8503be2d6ca5bef6394686b7fee9
Marcus Chiu <marcuschiu9@gmail.com>
2025-07-29 23:55:25 -0500

    initial commit

---
+++ src/main/java/com/example/spring_observability_zipkin/SpringObservabilityZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_observability_zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringObservabilityZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringObservabilityZipkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_observability_zipkin/SpringObservabilityZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_observability_zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservabilityZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
